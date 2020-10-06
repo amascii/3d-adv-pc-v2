bcddfc3cfc26f5c5a94986e15878e90a429168be
Armando Sierra <manguin98@gmail.com>
2020-10-06 19:37:52 +0900

    Update tf_nndistance_compile.sh

--- utils/tf_nndistance_compile.sh
+++ utils/tf_nndistance_compile.sh
@@ -1,3 +1,4 @@
-/usr/local/cuda-8.0/bin/nvcc tf_nndistance_g.cu -o tf_nndistance_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
+CUDA="10.0"
+PYTHON="3.6"
+/usr/local/cuda-$CUDA/bin/nvcc tf_nndistance_g.cu -o tf_nndistance_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /usr/local/lib/python$PYTHON/dist-packages/tensorflow/include -I /usr/local/cuda-$CUDA/include -I /usr/local/lib/python$PYTHON/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-$CUDA/lib64/ -L/usr/local/lib/python$PYTHON/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
